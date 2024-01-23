c170899af28d83ce4ca49c93349b8ddeafeccb4d
Sasikala <sasikala23july@gmail.com>
2024-01-23 13:44:17 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { AppComponent } from './app.component';
+import { EmptyComponent } from './empty/empty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'first',component: FirstComponent},
+  {path:'second',component:SecondComponent},
+  {path:'empty', component: EmptyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { EmptyComponent } from './empty/empty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    EmptyComponent
   ],
   imports: [
     BrowserModule,
